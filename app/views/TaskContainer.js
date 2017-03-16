2a099dc6cc9a4a15bb4aeef738ef41debacf324c
Macmini05 <macmini05@lancy.js>
2017-03-16 15:23:13 +0530

    Minor Changes

--- app/views/TaskContainer.js
+++ app/views/TaskContainer.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {
-    ScrollView
+    ScrollView,
+    Text,
+    StyleSheet
 } from 'react-native';
 import TodoStore from "../stores/TodoStore";
 import Task from './Task';
@@ -37,9 +39,20 @@ export default class TaskContainer extends Component {
     
     return (
       <ScrollView>
+        {todos.length === 0 && <Text style={styles.noData}>No Data</Text>}
       	{todos.map((data) => <Task key={data.id} {...data}/>)}
       </ScrollView>
     );
   }
 
 }
+
+const styles = StyleSheet.create({
+  noData: {
+    flex: 1,
+    marginTop: 50,
+    fontSize: 16,
+    textAlign: 'center',
+    fontWeight: '500'
+  }
+});
