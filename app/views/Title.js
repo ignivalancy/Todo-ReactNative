eb0ab25a5426efb2c7e7401891a06aba24001342
Macmini05 <macmini05@lancy.js>
2017-03-21 19:07:35 +0530

    Direct Disptach Action Test

--- app/views/Title.js
+++ app/views/Title.js
@@ -5,42 +5,53 @@ import {
     View,
     AlertIOS
 } from 'react-native';
-import { bindActionCreators } from 'redux'
+// import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import Button from '../components/Button';
 import * as TodoActions from "../actions/TodoActions";
 
 class Title extends Component {
 
+  reloadTodo = () => {
+
+      const { dispatch } = this.props;
+      dispatch(TodoActions.reloadTodo());
+
+  }
+
+  completeAll = () => {
+      const { dispatch } = this.props;
+      dispatch(TodoActions.completeAll());
+  }
+
   handleSave = text => {
 
     if (text.length !== 0) {
-
-      const { createTodo } = this.props.actions;
-      createTodo(text)
+      const { dispatch } = this.props;
+      dispatch(TodoActions.createTodo(text));
     }
 
   }
 
   createTodo = () => {
+
       AlertIOS.prompt(
           'Add new Task',
           'Save your next task in the list',
           this.handleSave
       );
+      
   }
 
   render() {
-
-    const { createTodo, completeAll, clearCompleted, reloadTodo } = this.props.actions;
 
     return (
       <View style={styles.titleBox}>
         <Text style={styles.title}>
           Flux App
         </Text>
-        <Button onClick={reloadTodo} icon="reload" iconSize={25} iconColor="#7d92da"/>
-        <Button onClick={completeAll} icon="notification-clear-all" iconSize={25} iconColor="#7d92da"/>
+        <Button onClick={this.reloadTodo} icon="reload" iconSize={25} iconColor="#7d92da"/>
+        <Button onClick={this.completeAll} icon="notification-clear-all" iconSize={25} iconColor="#7d92da"/>
         <Button onClick={this.createTodo} icon="plus" iconSize={25} iconColor="#7d92da"/>
       </View>
     );
@@ -65,11 +76,8 @@ const styles = StyleSheet.create({
   }
 });
 
-const mapDispatchToProps = dispatch => ({
-    actions: bindActionCreators(TodoActions, dispatch)
-})
+// const mapDispatchToProps = dispatch => ({
+//     actions: bindActionCreators(TodoActions, dispatch)
+// })
 
-export default connect(
-  null,
-  mapDispatchToProps
-)(Title)
+export default connect()(Title)
